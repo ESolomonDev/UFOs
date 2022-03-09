7ccf5b93621e306dc39bc4feacdd5fa45a723576
Eli Solomon <ESolomonDev@gmail.com>
2022-03-09 18:42:02 -0500

    added event listener for app.js

--- static/js/app.js
+++ static/js/app.js
@@ -29,3 +29,32 @@ function buildTable(data) {
     });//end outter for each
 
 }//end buildTable func scope
+
+//func event listener for click
+function handleClick(){
+    // Grab the datetime value from the filter
+    //get datetime in html tags and store in var
+    let date = d3.select("#datetime").property("value");
+    //asign tableData to a filteredData var
+    let filteredData = tableData;
+
+    // Check to see if a date was entered and filter the
+    // data using that date.
+    if(date){
+        //store filtered data into var matching date value
+        // Apply `filter` to the table data to only keep the
+        // rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
